b47d60e30f7dee53b31ea0163fc6edabe04de806
Rahul Prajapati <prajapati91032@gmail.com>
2021-07-27 14:30:37 +0530

    Swapping two and three variables

---
+++ swap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num = 0;
+    int num1 = 20;
+    int num2 = 40;
+    cout<<"Before swapping"<<endl;
+    cout<<"num1 "<<num1<<"\n"<<"num2 "<<num2<<endl;
+    num = num1;
+    num1 = num2;
+    num2 = num;
+    cout<<"After swapping"<<endl;
+    cout<<"num1 "<<num1<<"\n"<<"num2 "<<num2<<endl;
+}
---
+++ swap3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 20;
+    int num1 = 40;
+    int num2 = 60;
+    cout << "Before swapping" << endl;
+    cout << "num " << num << "\n"
+         << "num1 " << num1 << "\n"
+         << "num2 " << num2 << endl;
+    num = num + num1 + num2;
+    num1 = num - (num1+num2);
+    num2 = num- (num1+num2);
+    num = num - (num1+num2);
+    cout << "After swapping" << endl;
+    cout << "num " << num << "\n"
+         << "num1 " << num1 << "\n"
+         << "num2 " << num2 << endl;
+}
